ecdb674b70878d12f83757c6429e795a6d97ef83
Sulfurbottom <397love@gmail.com>
2025-06-16 08:11:12 +0900

    db/index.ts에서 스케줄러 기능 제거 및 코드 정리

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,13 +3,3 @@ import * as schema from "./schema";
 
 // Drizzle 인스턴스를 생성하는 함수
 export const createDb = (d1: D1Database) => drizzle(d1, { schema });
-
-export default {
-	async scheduled(
-		controller: ScheduledController,
-		env: Env,
-		ctx: ExecutionContext,
-	) {
-		console.log("cron processed");
-	},
-};
